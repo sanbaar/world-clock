eebd9c0b59d0580be32d877adb989363a0d466b5
sanbaar <sandra.baar88@gmail.com>
2023-03-08 19:33:12 +0100

    added real time

--- src/script.js
+++ src/script.js
@@ -6,8 +6,12 @@ let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesCurrentDate = moment()
   .tz("America/Los_Angeles")
   .format("dddd, MMMM Do");
-
 losAngelesDateElement.innerHTML = losAngelesCurrentDate;
+
+let losAngelesCurrentTime = moment()
+  .tz("America/Los-Angeles")
+  .format("hh:mm A");
+losAngelesTimeElement.innerHTML = losAngelesCurrentTime;
 
 //Tokyo
 let tokyoElement = document.querySelector("#tokyo");
@@ -15,8 +19,10 @@ let tokyoDateElement = tokyoElement.querySelector(".date");
 let tokyoTimeElement = tokyoElement.querySelector(".time");
 
 let tokyoCurrentDate = moment().tz("Asia/Tokyo").format("dddd, MMMM Do");
-
 tokyoDateElement.innerHTML = tokyoCurrentDate;
+
+let tokyoCurrentTime = moment().tz("Asia/Tokyo").format("hh:mm A");
+tokyoTimeElement.innerHTML = tokyoCurrentTime;
 
 //Melbourne
 let melbourneElement = document.querySelector("#melbourne");
@@ -26,8 +32,10 @@ let melbourneTimeElement = melbourneElement.querySelector(".time");
 let melbourneCurrentDate = moment()
   .tz("Australia/Melbourne")
   .format("dddd, MMMM Do");
-
 melbourneDateElement.innerHTML = melbourneCurrentDate;
+
+let melbourneCurrentTime = moment().tz("Australia/Melbourne").format("hh:mm A");
+melbourneTimeElement.innerHTML = melbourneCurrentTime;
 
 //Calcutta
 let calcuttaElement = document.querySelector("#calcutta");
@@ -35,5 +43,7 @@ let calcuttaDateElement = calcuttaElement.querySelector(".date");
 let calcuttaTimeElement = calcuttaElement.querySelector(".time");
 
 let calcuttaCurrentDate = moment().tz("Asia/Kolkata").format("dddd, MMMM Do");
-
 calcuttaDateElement.innerHTML = calcuttaCurrentDate;
+
+let calcuttaCurrentTime = moment().tz("Asia/Kolkata").format("hh:mm A");
+calcuttaTimeElement.innerHTML = calcuttaCurrentTime;
